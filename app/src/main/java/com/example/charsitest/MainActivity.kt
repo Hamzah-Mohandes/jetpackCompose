bddfd469b1c70bcea7c2ad3c5b238f20a3956a8f
Hamzah <rootmoodrm@gmail.com>
2025-04-08 00:50:08 +0200

    Feat: Implement various button types in Compose UI
    This commit introduces different button types in the Compose UI, including `Button`, `OutlinedButton`, `IconButton`, and `TextButton`. The layout is updated to arrange buttons vertically with spacing and horizontal alignment. The `IconButton` now displays a filled heart icon, and the `TextButton` combines text with a heart icon. A simple `Button` with a heart icon is also added.

--- app/src/main/java/com/example/charsitest/MainActivity.kt
+++ app/src/main/java/com/example/charsitest/MainActivity.kt
@@ -4,17 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,45 +39,51 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp() {
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .padding(16.dp)
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+        verticalArrangement = Arrangement.spacedBy(25.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+
     ) {
-        Text(
-            text = "Hello",
-            color = Color.White, // Sets the font color to white
-            fontSize = 52.sp,
-            modifier = Modifier
-                 .padding(16.dp) // das ist margin
-                .background(Color.Black)
-                .padding(16.dp)
-
-                //.padding(16.dp)
-//                .width(200.dp)
-//                .height(100.dp)
-//                .size(200.dp)
-//                .fillMaxWidth(1f)
-//                .fillMaxSize(0.6F)
-//                .fillMaxHeight()
-        )
-        Box (modifier = Modifier
-            .padding(16.dp)
-            .size(100.dp)
-            .rotate(45f) // Rotates the box by 45 degrees
-            .alpha(0.5f) // Sets the opacity to 50%
-            .background(Color.Red)
-
-
-        ) {
-
+        Button(onClick = { }) {
+            Text(text = "Simple Button", fontSize = 20.sp)
         }
+
+        OutlinedButton(onClick = { }) {
+            Text(text = "Outlined Button", fontSize = 20.sp)
+        }
+
+        IconButton( onClick = { }) {
+            Icon(imageVector = Icons.Filled.Favorite, contentDescription = "Favorite",
+                modifier = Modifier
+                    .padding(10.dp),
+                tint = Color.Red
+                )
+        }
+
+        TextButton( onClick = { }) {
+            Text(text = "Text like Button   ", fontSize = 20.sp)
+            Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
+            Icon(imageVector = Icons.Filled.Favorite, contentDescription = "Favorite",
+                modifier = Modifier
+                    .size(ButtonDefaults.IconSize),
+                tint = Color.Red
+            )
+        }
+
+        Button( onClick = { }) {
+            Text(text = "Simple Button", fontSize = 20.sp)
+            Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
+            Icon( imageVector = Icons.Filled.Favorite, contentDescription = "Favorite",)
+        }
+
     }
 }
-
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MyApp()
-
 }
