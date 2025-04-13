ed643bc5d27c3e1dee9328af30f5b0e4b9fd5aab
Hamzah <rootmoodrm@gmail.com>
2025-04-13 23:27:02 +0200

    Feat: Implement text input field in Compose UI
    This commit introduces a `TextField` in the Compose UI, allowing users to input text. The `TextField` includes a label indicating "Enter your Email ...". The layout is updated to center the `TextField` within a `Box`. The state of the text input is managed using `mutableStateOf` and `remember` to handle text changes dynamically. Additionally, the commit removes the preloaded font and the string resource "mkr".

--- app/src/main/java/com/example/charsitest/MainActivity.kt
+++ app/src/main/java/com/example/charsitest/MainActivity.kt
@@ -1,25 +1,23 @@
 package com.example.charsitest
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,23 +30,30 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp() {
-    Column (modifier = Modifier.fillMaxSize()
-        .padding(25.dp)
-    )
-    {
-        Text(stringResource(id = R.string.mkr).repeat(30),
-            color = Color.DarkGray,
-            //fontSize = 24.sp,
-            fontStyle = FontStyle.Italic,
-            fontWeight = FontWeight.ExtraBold,
-            fontFamily = FontFamily.Cursive,
-            //textAlign = TextAlign.Center
-            lineHeight = 40.sp,
-            maxLines = 3,
+fun MyApp()
 
+{
+    Box (modifier = Modifier
+        .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ){
+
+        var newText by remember {
+            mutableStateOf("")
+        }
+
+        Column (modifier = Modifier.fillMaxSize()
+            .padding(25.dp)
         )
+        {
+            TextField(value = newText, onValueChange = {
+                nT ->
+                newText = nT
+            },
+                label = { Text(text = "Enter your Email ... ") },
+                maxLines = 6
 
+                )}
     }
 
 }
