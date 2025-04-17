4c484c25ee3ac2761e76f88720c100f4c497a2ca
Hamzah <rootmoodrm@gmail.com>
2025-04-17 12:14:20 +0200

    Feat: Implement TextField, OutlinedTextField, and BasicTextField in Compose UI
    This commit introduces three types of text input fields in the Compose UI: `TextField`, `OutlinedTextField`, and `BasicTextField`.
    -The `TextField` includes a label, placeholder, leading icon, and an optional trailing icon button that displays a Toast with the input text. It also customizes text colors and handles keyboard actions like hiding the keyboard and clearing focus.
    -The `OutlinedTextField` is similar but uses an outlined style and includes options for keyboard capitalization, type, and IME action.
    -The `BasicTextField` is a simpler text input field with a yellow background and specified dimensions. The layout is organized vertically with spacing and centers content both horizontally and vertically.

--- app/src/main/java/com/example/charsitest/MainActivity.kt
+++ app/src/main/java/com/example/charsitest/MainActivity.kt
@@ -4,18 +4,26 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Email
 import androidx.compose.material.icons.outlined.Send
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -24,7 +32,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -38,36 +51,63 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+/**
+ * Haupt-Composable-Funktion, die das UI der App definiert.
+ */
 @Composable
 fun MyApp()
 
 {
     Box (modifier = Modifier
-        .fillMaxSize(),
+        .fillMaxSize()
+
+        , // Zentriert den Inhalt innerhalb des Box-Containers.
         contentAlignment = Alignment.Center
     ){
-
+        // Ruft den aktuellen Kontext ab, der für Toasts verwendet wird.
         var context = LocalContext.current
+
+        // Zustand für den Text im ersten Textfeld.
         var newText by remember {
             mutableStateOf("")
         }
-
-        Column (modifier = Modifier.fillMaxSize(),
-            Arrangement.Center,
-            Alignment.CenterHorizontally,
+        // Zustand für den Text im zweiten Textfeld.
+        var newText2 by remember {
+            mutableStateOf("")
+        }
+        // Controller für die Softwaretastatur.
+        var keyboardController = LocalSoftwareKeyboardController.current
+        // Manager für den Fokus.
+        val focusManager = LocalFocusManager.current
+        // Definiert eine vertikale Anordnung von UI-Elementen.
+        Column (modifier = Modifier.fillMaxSize()
+            ,
+            Arrangement.Center, // Zentriert die Elemente vertikal.
+            Alignment.CenterHorizontally, // Zentriert die Elemente horizontal.
         )
         {
+            // Erstes Textfeld
             TextField(value = newText, onValueChange = {
                 nT ->
                 newText = nT
             },
+                // Gibt den Label-Text an.
                 label = { Text(text = "Enter your Email ... ") },
-               // maxLines = 6,
-                singleLine = true,
-                modifier = Modifier.width(280.dp),
+                // Legt die Farben des Textfeldes fest.
+                colors = TextFieldDefaults.colors(
+                    focusedTextColor = androidx.compose.ui.graphics.Color.Blue,
+                    unfocusedTextColor = androidx.compose.ui.graphics.Color.Gray,
+                    focusedLabelColor = androidx.compose.ui.graphics.Color.Blue,
+                    unfocusedLabelColor = androidx.compose.ui.graphics.Color.Gray,
+
+                ), // maxLines = 6,
+                singleLine = true, // Das Textfeld kann nur eine Zeile haben
+                modifier = Modifier.width(280.dp), // Setzt die Breite des Textfeldes
+                // Platzhalter Text
                 placeholder = {
                     Text(text = "abs@gmail.com")
                 },
+                // Icon am Anfang des Textfeldes
                 leadingIcon = {
                     Icon(imageVector = Icons.Outlined.Email, contentDescription = "Email Icon")
                 },
@@ -79,7 +119,57 @@ fun MyApp()
                     }
                 }
 
-                )}
+                )
+            // Abstand zwischen den Elementen
+            Spacer(modifier = Modifier.padding(10.dp))
+            // Zweites Textfeld
+            OutlinedTextField(value = newText2, onValueChange = {
+                nT ->
+                newText2 = nT
+            },
+                // Gibt den Label-Text an.
+                label = { Text(text = "Enter your Email ... ") },
+                // maxLines = 6,
+                singleLine = true, // Das Textfeld kann nur eine Zeile haben
+                modifier = Modifier.width(280.dp), // Setzt die Breite des Textfeldes
+                placeholder = { // Platzhalter Text
+                    Text(text = "abs@gmail.com")
+                },
+                leadingIcon = {
+                    Icon(imageVector = Icons.Outlined.Email, contentDescription = "Email Icon")
+                },
+                trailingIcon = {
+                    IconButton(onClick = {
+                        Toast.makeText(context, newText2, Toast.LENGTH_SHORT).show()
+                    }) {
+                       Icon(imageVector = Icons.Outlined.Send, contentDescription = "send Icon")
+                    }
+                },
+                // Definiert Optionen für die Tastatur
+                keyboardOptions = KeyboardOptions(
+                    // capitalization = KeyboardCapitalization.Characters
+                    capitalization = KeyboardCapitalization.Sentences,// Beginnt jeden Satz mit einem Großbuchstaben.
+                    keyboardType = androidx.compose.ui.text.input.KeyboardType.Phone,// Zeigt die Telefontastatur an.
+                    imeAction = ImeAction.Send,// Zeigt die "Senden"-Aktion auf der Tastatur an.
+
+                ),
+                // Definiert die Aktionen, die beim Drücken der Tasten auf der Tastatur ausgeführt werden.
+                keyboardActions = KeyboardActions(
+                   onSend = {
+                       keyboardController?.hide()// Versteckt die Tastatur.
+                       Toast.makeText(context, "send Button clicked", Toast.LENGTH_SHORT).show()// Zeigt einen Toast an.
+                       focusManager.clearFocus()// Entfernt den Fokus vom Textfeld.
+                   }
+                )
+
+                )
+            // Abstand zwischen den Elementen
+            Spacer(modifier = Modifier.padding(40.dp))
+            // Einfaches Textfeld
+            BasicTextField(value = newText, onValueChange = {newText = it},
+                modifier = Modifier.width(280.dp).height(100.dp).background(Color.Yellow) // Setzt Breite, Höhe und Hintergrundfarbe
+                )
+        }
     }
 
 }
