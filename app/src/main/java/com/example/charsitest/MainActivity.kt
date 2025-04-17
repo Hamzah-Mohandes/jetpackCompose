7903dee263acc26348d0f5a326b0d27ad2353988
Hamzah <rootmoodrm@gmail.com>
2025-04-17 11:33:06 +0200

    Feat: Enhance TextField with icons and interaction
    This commit enhances the TextField by adding leading and trailing icons. The leading icon is an email icon, and the trailing icon is a send icon button. Clicking the send icon button triggers a Toast message displaying the entered text. The TextField's width is set to 280dp, and a placeholder "abs@gmail.com" is added. The `maxLines` property is removed, and `singleLine` is set to `true`. The parent `Column` now arranges its children in the center and aligns them horizontally.

--- app/src/main/java/com/example/charsitest/MainActivity.kt
+++ app/src/main/java/com/example/charsitest/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.charsitest
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Email
+import androidx.compose.material.icons.outlined.Send
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -16,6 +24,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -38,12 +47,14 @@ fun MyApp()
         contentAlignment = Alignment.Center
     ){
 
+        var context = LocalContext.current
         var newText by remember {
             mutableStateOf("")
         }
 
-        Column (modifier = Modifier.fillMaxSize()
-            .padding(25.dp)
+        Column (modifier = Modifier.fillMaxSize(),
+            Arrangement.Center,
+            Alignment.CenterHorizontally,
         )
         {
             TextField(value = newText, onValueChange = {
@@ -51,7 +62,22 @@ fun MyApp()
                 newText = nT
             },
                 label = { Text(text = "Enter your Email ... ") },
-                maxLines = 6
+               // maxLines = 6,
+                singleLine = true,
+                modifier = Modifier.width(280.dp),
+                placeholder = {
+                    Text(text = "abs@gmail.com")
+                },
+                leadingIcon = {
+                    Icon(imageVector = Icons.Outlined.Email, contentDescription = "Email Icon")
+                },
+                trailingIcon = {
+                    IconButton(onClick = {
+                        Toast.makeText(context, newText, Toast.LENGTH_SHORT).show()
+                    }) {
+                       Icon(imageVector = Icons.Outlined.Send, contentDescription = "send Icon")
+                    }
+                }
 
                 )}
     }
